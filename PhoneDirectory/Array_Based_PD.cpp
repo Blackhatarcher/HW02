6b6f98a4b8cf21c3762cab8a4e29e5dc69bf89a7
Blackhat-PC\Blackhat <christianwilliams20@my.whitworth.edu>
2017-02-17 12:36:30 -0800

    started on 1.7
    implementing remove entry method on line 117

--- PhoneDirectory/Array_Based_PD.cpp
+++ PhoneDirectory/Array_Based_PD.cpp
@@ -113,15 +113,21 @@ void Phone_Directory::save()
 	@return That person's name or an empty string
 	if not in the directory
 	*/
-string Phone_Directory::remove_entry(const string& name) // Exercise 1.7: please complete the remove_entry() method - Ed/Kent
+
+//++++++++++++++++++++++++++Christian Williams has Started to Work ++++++++++++++++++++++
+// Exercise 1.7: please complete the remove_entry() method - Ed/Kent
+string Phone_Directory::remove_entry(const string& name) 
 {
 
 	// Hint: you can use the code below to shift names down in the directory to remove the selected entry specified by "index"
-	// for (int i = index; i < size - 1; i++)
-		// the_directory[i] = the_directory[i + 1];
+	int index = 0;
+	for (int i = index; i < size - 1; i++)
+		the_directory[i] = the_directory[i + 1];
 
 	return "";
 }
+
+//++++++++++++++++++++++++++++Christian Williams has stopped working +++++++++++++++++++++
 
 // Private method implementation
 
